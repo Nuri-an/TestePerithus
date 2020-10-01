fd32d5c480423c5f975e8497b11ad321ef3783f5
nurian coelho <nuriancoelho@hotmail.com>
2020-10-01 16:49:05 -0300

    Arumando nome

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,10 @@ const express = require('express');
 
 const routes = express.Router();  
 
-const searchCitysController = require('./controllers/searchCitysController');
+const searchCitiesController = require('./controllers/searchCitiesController');
 const citiesController = require('./controllers/citiesController');
 
 routes.get('/', citiesController.index); //busca e retorna todos os estados
-routes.get('/city', searchCitysController.show); //busca e retorna as cidades localizadas no estado informado
+routes.get('/city', searchCitiesController.show); //busca e retorna as cidades localizadas no estado informado
 
 module.exports = routes;  
